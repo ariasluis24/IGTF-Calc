36fb91a9daac308f452a6ec1a37341ae4ba14bf8
Koma07 <ariasluis2406@gmail.com>
2024-03-16 20:25:20 -0400

    Changes to main.py Added return values for future fixes. Commented unused console text. Fixed parameters values on calc functions. Fixed values used to make the calcs whitin the functions. Added locale format to the amount payed on dollars. Changed functions to save code. WIP

--- main.py
+++ main.py
@@ -6,45 +6,56 @@ def principal_Calculation(sub_total):
 
     IVA = sub_total * 0.16
     total = sub_total + IVA
-    print(f'\nSub-Total:    {round(sub_total,2):n}')
+    print(f'\nSub-Total:    {round(sub_total,3):n}')
     print(f' IVA(16%):     {round(IVA,2):n}')
     print(f'    Total:    {round(total,2):n}\n')
-    return total
+    return total, IVA
+
+def dollars_Calculation(pay_Cash):
+    IGTF = pay_Cash * 0.03
+    # print("\n************************************")
+    # print(f'\nThe IGTF is:    {IGTF:n}')
+    # print("\n************************************")
+    return IGTF
 
 def calc_Retention(sub_total, iva ):
     iva_25 = iva * 0.25
     total_reten = sub_total + iva_25
-    print("\n************************************")
-    print(f'\nThe total of bill with retention is: {total_reten:n}')
-    print("\n************************************")
+    # print("\n************************************")
+    # print(f'\nThe total of bill with retention is: {total_reten:n}')
+    # print("\n************************************")
     return total_reten
 
-def dollars_Calculation(pay_Cash):
-    IGTF = pay_Cash * 0.03
-    print("\n************************************")
-    print(f'\nThe IGTF is:    {IGTF:n}')
-    print("\n************************************")
-    return IGTF
 
-def show_Reten_Bill_Details(sub_total, iva, igtf):
-    print("THE DETAILS OF THE RETENTION BILL")
-    print(f'Sub-Total:        {round(sub_total,2):n}')
-    print(f' IVA(25%):          {round(iva,2)*0.25:n}')
-    print(f'Retention Total:  {round(sub_total + iva,2):n}')
-    print(f'IGTF: {pay_Cash} (3%): {round(igtf,2):n}')
+def show_Reten_Bill_Details(sub_total, reten_iva, igtf):
+    print("\n\nTHE DETAILS OF THE RETENTION BILL")
+    print(f'Sub-Total:        ${round(sub_total,2):n}')
+    print(f' IVA(25%):          ${round(reten_iva,2):n}')
+    print(f'Retention Total:  ${round(sub_total + reten_iva,2):n}')
+    print(f'IGTF: ${pay_Cash:n} (3%): ${round(igtf,2):n}')
     print("\n************************************\n")
-    print(f'Retention Total + IGTF:  {round(total + igtf,2):n}')
+    print(f'Retention Total + IGTF:  ${round(sub_total + reten_iva + igtf,2):n}')
     print("\n************************************")
+    print(f"The rest to pay is: ${pay_Cash - (sub_total + reten_iva + igtf):n}")
 
-def show_Total_Bill_Details(sub_total,iva,total,igtf):
-    print("THE DETAILS OF THE TOTAL BILL")
-    print(f'Sub-Total:    {round(sub_total,2):n}')
-    print(f' IVA(16%):    {round(iva,2):n}')
-    print(f'    Total:    {round(total,2):n}')
-    print(f'IGTF: {pay_Cash}(3%): {round(igtf,2):n}')
-    print("\n************************************")
-    print(f'Total + IGTF: {round(total + igtf,2):n}')
-    print("\n************************************")
+def show_Bill_Detail(agent_of_retention,sub_total, iva, IGTF):
+    if len(agent_of_retention) == 1 and agent_of_retention.lower() == 'y':
+        total_retention = calc_Retention(sub_total, IVA) - sub_total
+        show_Reten_Bill_Details(sub_total, total_retention,IGTF)
+        pass
+    elif len(agent_of_retention) == 1 and agent_of_retention.lower() == 'n':
+        print('\nCalculation of retention pass\n')
+        print("THE DETAILS OF THE TOTAL BILL")
+        print(f'Sub-Total:    ${round(sub_total,2):n}')
+        print(f' IVA(16%):    ${round(iva,2):n}')
+        print(f'    Total:    ${round(total,2):n}')
+        print(f'IGTF: {pay_Cash:n} (3%): ${round(IGTF,2):n}')
+        print("\n************************************\n")
+        print(f'Total + IGTF: ${round(total + IGTF,2):n}')
+        print("\n************************************")
+        print(f"The rest to pay is: ${pay_Cash - (total + IGTF):n}\n")
+    pass
+
 while True:
     #Variables WIP
     BCV_Rate = 36.2760
@@ -70,16 +81,8 @@ while True:
         pay_Cash = float(input('Please how much is going to be cancel on $?: '))
         IGTF = dollars_Calculation(pay_Cash)
 
-        if len(agent_of_retention) == 1 and agent_of_retention.lower() == 'y':
-            # iva_retention = calc_Retention(sub_total, IVA) / 1.25
-            calc_Retention(sub_total, IVA)
-            show_Reten_Bill_Details(sub_total, IVA,IGTF)
-            break
-        elif len(agent_of_retention) == 1 and agent_of_retention.lower() == 'n':
-            print('Calculation of retention pass')
-        pass
+        show_Bill_Detail(agent_of_retention,sub_total,IVA,IGTF)
 
-        show_Total_Bill_Details(sub_total,IVA,total,IGTF)
         pass
 
     elif len(main_Question) == 1 and main_Question.lower() == 'n':
