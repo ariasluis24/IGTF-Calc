f2b480688ed0aeb97c654c0047d76a278f50ffe1
Luis Arias <ariasluis2406@gmail.com>
2024-03-21 13:31:09 -0400

    Fixed floating point problem. Imported Decimal function from decimal module. Changed number of decimals wanted from some round functions. Created function to calculate the rest to pay of the user. Added function to calculate the rest to pay into the main function. Fixed problem on the bill format, to be accurate, on the decimals, using the Decimal function on some values, meanwhile using the round the extended Decimal result.

--- calculations.py
+++ calculations.py
@@ -1,11 +1,12 @@
 # Import to set the locale system for de decimal(,) and thousand(.) separators.
 import locale
+from decimal import Decimal
 locale.setlocale(locale.LC_ALL, 'en_DE')
 
 def principal_Calculation(sub_total):
     IVA = sub_total * 0.16
     total = sub_total + IVA
-    print(f'\nSub-Total:    {round(sub_total,3):n}')
+    print(f'\nSub-Total:    {round(sub_total,2):n}')
     print(f' IVA(16%):     {round(IVA,2):n}')
     print(f'    Total:    {round(total,2):n}\n')
     return IVA, total
@@ -41,8 +42,10 @@ def question_Loop(question):
         else:
             print('Please select a valid option...')
 
-def rest_to_Pay(pay_cash,total):
-    # print(f'The resto to pay is:{}')
+def rest_to_Pay(pay_Cash, IGTF,total):
+    print('******************************')
+    print(f'The rest to pay is:{round((total + IGTF) - pay_Cash,2):n}')
+    print('******************************')
     pass
 
 def main_Function(main_Question, agent_of_retention, sub_total, IVA,total):
@@ -53,11 +56,12 @@ def main_Function(main_Question, agent_of_retention, sub_total, IVA,total):
         pay_Cash = is_Float_Loop('Please how much is going to be cancel on $?: ')
         IGTF = calc_IGTF(pay_Cash)
         print_Reten_Bill(sub_total, reten_IVA, pay_Cash, IGTF, reten_Total)
-
+        rest_to_Pay(pay_Cash, IGTF,reten_Total)
     elif len(main_Question) == 1 and main_Question.lower() == 'y' and agent_of_retention.lower() == 'n':
         pay_Cash = is_Float_Loop('Please how much is going to be cancel on $?: ')
         IGTF = calc_IGTF(pay_Cash)
         print_Bill(sub_total, IVA, pay_Cash,IGTF,total)
+        rest_to_Pay(pay_Cash, IGTF,total)
     elif len(main_Question) == 1 and main_Question.lower() == 'n' and agent_of_retention.lower() == 'y':
         
         reten_Results = calc_Retention(sub_total,IVA)
@@ -70,16 +74,16 @@ def main_Function(main_Question, agent_of_retention, sub_total, IVA,total):
         print('Please select a valid option...')
 
 def print_Bill(sub_total, IVA, pay_Cash,IGTF, total):
-    print(f'\nSub-Total:    ${round(sub_total,3):n}')
+    print(f'\nSub-Total:    ${round(sub_total,2):n}')
     print(f' IVA(16%):     ${round(IVA,2):n}')
     print(f'$ Payment:    ${round(pay_Cash,2):n}')
     print(f' IGTF(3%):     ${round(IGTF,2):n}')
-    print(f'    Total:    ${round(total+ IGTF,2):n}\n')
+    print(f'    Total:    ${round(Decimal(total) + Decimal(IGTF),2):n}\n')
 
 # TODO Use just one format for the bill, switching the iva percentage and value, depending if the user wants to calculate the retention of the iva.
 def print_Reten_Bill(sub_total, reten_IVA, pay_Cash,IGTF,reten_Total):
-    print(f'\n        Sub-Total:     ${round(sub_total,3):n}')
+    print(f'\n        Sub-Total:     ${round(sub_total,2):n}')
     print(f'        IVA (25%):      ${round(reten_IVA,2):n}')
     print(f'        $ Payment:      ${round(pay_Cash,2):n}')
     print(f'         IGTF(3%):      ${round(IGTF,2):n}')
-    print(f'            Total:     ${round(reten_Total+ IGTF,2):n}\n')
+    print(f'            Total:     ${round(Decimal(reten_Total) + Decimal(IGTF),2):n}\n')
