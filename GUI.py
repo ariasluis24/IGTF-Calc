964fee90e2bc93a922136713b9568c288f5e63b9
Koma07 <ariasluis2406@gmail.com>
2024-04-20 16:46:39 -0400

    Created GUI version alpha 1.0. Moved calculations.py & icon.png into the src folder. Added files to .gitonore. Deleted unused functions into calculations.py. Added GUI.py with all the new Graphical User Interface. Created on GUI.py 2 new functions to print and convert the values entered by the user. Refactored impot of calculations into GUI.py

---
+++ GUI.py
@@ -0,0 +1,190 @@
+from tkinter import *
+from tkinter import simpledialog
+import tkinter.messagebox
+import os, src.calculations as calculations
+
+# Variables
+user_name = os.getlogin()
+result_height = 14 # Used into the Labels.
+
+
+def get_sub_total():
+    """
+    # This function works in the next way.
+    # Is called on the button command of 'calculate_button'.
+    # Create 3 global variables.
+    # Gets the value of the entry 'sub_total'. Converts the value into float and storage it into the 'sub_total_float' variable.
+    # Calls the module 'calculations' to use 'principal_Calculation' and calculate
+      the main values using 'sub_total_float' as an argument and storage them into 2 separate variables.
+     
+    # Calls the module 'calculations' to use 'GUI_principal_Calculation' and storage the returned 
+      text value on the 'principal_calculation' variable, using the previous variables 'sub_total_float', 'IVA', 'total'.
+    
+    # Creates a Label to print the 'GUI_principal_Calculation' returned text, storage into 'principal_calculation' variable.
+      The label contains its own configuration such as font, border, width, height, relief. And its put it into the window 
+      using grid.
+    
+    # 2 tkinter.messageboxes, asking a yes/no question are created saving their values into the 2 global variables for further use.
+    
+    # Now depending on the values of the tkinter.messageboxes the program would ask using a simpledialog for a value, which is goin
+      to be saved as a float.
+      Then we again call the calculations module this time to use the 'calc_IGTF' function, and save its value to the variable 'IGTF'.
+      We call again the calculations module, this time to calculate the retention of the values. 
+      And save the return values into 'reten_IVA' & 'reten_Total'.
+      With those 2 values we then call again the calculations module, to use the 'GUI_print_Reten_Bill', using as arguments 
+      the previous variables (sub_total_float, reten_IVA, pay_cash, IGTF, reten_Total), saving the return value into 'reten_result'.
+     
+    # Then the variables used as arguments on the 'calculations.GUI_print_Reten_Bill' are saved into the global variables 'my_list'.
+    
+    # Now the value saved into 'reten_result' variable, is used into a Label to print the result into the main window. Using again grid.
+    #
+    # After the label is shown into the window the state of the 'change_rate_button' is change from: DISABLE into 'normal'.
+
+    # The rest of the function is the same but just depending on the values of the 'main_question' & 'agent_of_retention'.
+      It would use the module calculations 'GUI_print_Reten_Bill' or 'GUI_print__Bill'.
+      Saving an individual 'my_list' for each case, to be use further into the program.
+
+    """
+
+    global my_list, main_question, agent_of_retention
+    sub_total_float = float(sub_total.get())
+    IVA, total = calculations.principal_Calculation(sub_total_float)
+    principal_calculation = calculations.GUI_principal_Calculation(sub_total_float, IVA, total)
+    Label(window, text=principal_calculation, font=('Roboto', 10), justify=RIGHT, borderwidth=1,width=60, height=result_height, relief='solid' ).grid(row=3)
+    main_question = tkinter.messagebox.askquestion('',"This bill is going to be cancel with $?")
+    agent_of_retention = tkinter.messagebox.askquestion('', "Do you wanna calculate the retention?")
+    if main_question == 'yes' and agent_of_retention == 'yes':
+        pay_cash = simpledialog.askfloat("Input", "Please how much is going to be cancel on $?:")
+        
+        IGTF = calculations.calc_IGTF(pay_cash)
+        reten_IVA, reten_Total = calculations.calc_Retention(sub_total_float, IVA)
+        reten_result = calculations.GUI_print_Reten_Bill(sub_total_float, reten_IVA, pay_cash, IGTF, reten_Total)
+        my_list = [sub_total_float, IVA, pay_cash, IGTF, total]
+
+        Label(window, text=reten_result, font=('Roboto', 10), justify=RIGHT, borderwidth=1,width=70, height=result_height, relief='solid', bg='yellow').grid(row= 3, column=1)
+        change_rate_button.config(state='normal')
+        
+    elif main_question == 'yes' and agent_of_retention == 'no':
+        pay_cash = simpledialog.askfloat("Input", "Please how much is going to be cancel on $?:")
+
+        IGTF = calculations.calc_IGTF(pay_cash)
+        result = calculations.GUI_print_Bill(sub_total_float, IVA, pay_cash, IGTF, total) 
+        my_list = [sub_total_float, IVA, pay_cash, IGTF, total]
+
+        Label(window, text=result, font=('Roboto', 10), justify=RIGHT, borderwidth=1,width=70, height=result_height, relief='solid', bg='yellow').grid(row= 3, column=1)
+        change_rate_button.config(state='normal')
+    
+    elif main_question == 'no' and agent_of_retention == 'yes':
+        reten_IVA, reten_Total = calculations.calc_Retention(sub_total_float, IVA)
+        reten_result = calculations.GUI_print_Reten_Bill(sub_total_float,reten_IVA,0,0,reten_Total)
+        my_list = [sub_total_float, IVA, 0, 0, total]
+
+        Label(window, text=reten_result, font=('Roboto', 10), justify=RIGHT, borderwidth=1,width=70, height=result_height, relief='solid', bg='yellow').grid(row= 3, column=1)
+        change_rate_button.config(state='normal')
+    
+    
+    elif main_question == 'no' and agent_of_retention == 'no':
+        result = calculations.GUI_print_Bill(sub_total_float, IVA, 0, 0, total)
+        my_list = [sub_total_float, IVA, 0, 0, total]
+
+        Label(window, text=result, font=('Roboto', 10), justify=RIGHT, borderwidth=1,width=70, height=result_height, relief='solid', bg='yellow').grid(row= 3, column=1)
+        change_rate_button.config(state='normal')
+
+def clicked(): 
+    """ 
+    This function works in the next way. 
+    Is called on the button command of 'chang_rate_button'.
+    Check the value of the global variable 'agent_of_retention'.
+    Depending on the value of 'agent_of_retention' it would take a 'my_list' of the previous function and do the next:
+
+    Case 1 'agent_of_retention' == 'no':
+        It would call the calculations module 
+        to use the 'convert_to_bolivars' function using the variables 'my_list' & 'calculation.price_BCV' as arguments,
+        ('my_list' values would depend on the selection of 'main_question' & 'agent_of_retention' on the previous function).
+        saving the return values into a separate variables (sub_total_BS,  IVA_BS, pay_cash_BS, IGTF_BS, total_BS).
+        
+        Then calling again the calculations module we would use the 'GUI_print_Bill'. Using as arguments the separate variables
+        (sub_total_BS,  IVA_BS, pay_cash_BS, IGTF_BS, total_BS)
+        Saving the returned text into a variable called 'result_BS'.
+        Then created a Label similiar to the previous function, replacing those values with the converted ones.
+         
+
+    Case 2 'agent_of_retention' == 'yes':
+        It would call the calculations module 
+        to use the 'convert_to_bolivars' function using the variables 'my_list' & 'calculation.price_BCV' as arguments,
+        ('my_list' values would depend on the selection of 'main_question' & 'agent_of_retention' on the previous function).
+        saving the return values into a separate variables (sub_total_BS,  IVA_BS, pay_cash_BS, IGTF_BS, total_BS).
+        
+        Then calling again the calculations module we would use the 'GUI_print_Reten_Bill'. Using as arguments the separate variables
+        (sub_total_BS,  IVA_BS, pay_cash_BS, IGTF_BS, total_BS)
+        Saving the returned text into a variable called 'result_reten_BS'.
+        Then created a Label similiar to the previous function, replacing those values with the converted ones.
+    
+    """
+    if agent_of_retention == 'no':
+        sub_total_BS,  IVA_BS, pay_cash_BS, IGTF_BS, total_BS = calculations.convert_to_Bolivars(my_list , calculations.price_BCV)
+        results_BS = calculations.GUI_print_Bill(sub_total_BS ,IVA_BS, pay_cash_BS, IGTF_BS, total_BS)
+        Label(window, text=results_BS, font=('Roboto', 10), justify=RIGHT, borderwidth=1,width=70, height=result_height, relief='solid', bg='yellow').grid(row= 3, column=1)
+
+    if agent_of_retention == 'yes':
+        sub_total_BS,  IVA_BS, pay_cash_BS, IGTF_BS, total_BS = calculations.convert_to_Bolivars(my_list , calculations.price_BCV)
+        results_reten_BS = calculations.GUI_print_Reten_Bill(sub_total_BS ,IVA_BS, pay_cash_BS, IGTF_BS, total_BS)
+        Label(window, text=results_reten_BS, font=('Roboto', 10), justify=RIGHT, borderwidth=1,width=70, height=result_height, relief='solid', bg='yellow').grid(row= 3, column=1)
+
+# Windows entity  
+window = Tk()
+
+# Config of window
+icon = PhotoImage(file='src\\icon.png')
+window.title('IGTF Calculator')
+window.iconphoto(True, icon)
+window.geometry('800x360')
+
+# Creating Labels
+question = Label(window, 
+                text='Please how much is the sub total on $?:',
+                font=('Roboto', 14),
+                justify='left',
+                width=35)
+sub_total = Entry(window, 
+                  font=('Arial', 18),
+                  width=20,
+                  bg='red',
+                  justify='center')
+result = Label(window, 
+                text='result',
+                font=('Roboto', 14),
+                relief='raised',
+                justify='left',
+                anchor='w',
+                bg='red',
+                width=50  )
+user = Label(window, 
+             text=user_name, 
+             font=('Roboto', 24), 
+             activebackground='blue')
+version = Label(window, text='Version: alpha-1.0')
+
+# Creating Buttons
+calculate_button = Button(window, text='Calculate', command=get_sub_total, width=15)
+delete_button = Button(window, text='Deleted')
+export_button = Button(window, text='Export')
+change_rate_button = Button(window, text='$ / BS', font=('Roboto', 12), command=clicked, state=DISABLED)
+
+# Position of Widgets
+question.grid(row=0 , column=0, sticky='nwse')
+sub_total.grid(row=1, column=0, sticky='nwe')
+sub_total.focus_set() # Focus the entry on the window to instanly put a value.
+calculate_button.grid(row=2, column=0, sticky='nwse')
+change_rate_button.grid(row=5, column=1, padx= 2, pady=2, sticky='se')
+version.grid(row=5, column=0, sticky='ws')
+
+# Config Grid of the window
+window.columnconfigure((0,1), weight= 1, uniform= 'a')
+window.rowconfigure(4, weight= 10, uniform= 'a')
+# window.rowconfigure(0, weight=1, uniform='b')
+# window.rowconfigure(1, weight=1, uniform='b')
+# window.rowconfigure(2, weight=3, uniform='b')
+
+# Run
+window.mainloop()
--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 #IGTF-Calc 
-#ver: alpha-0.77 
-import calculations, datetime
+#ver: alpha-1.00
+import datetime
 now = datetime.datetime.now()
 date = now.strftime("%d-%m-%Y %H-%M-%S")
-while True:
-    # Variables WIP
-    name_file = f'Bill-{date}.txt'
-    # Program WIP
-    sub_total = calculations.is_Float_Loop('Please how much is the sub total on $?: ')
-    # Calculations
-    IVA, total = calculations.principal_Calculation(sub_total)
-    # TODO Create program to create an external PDF file.
-    # Write method to write file in an external txt file.
-    with open(f'Bills\\Bill-{sub_total:n}$ {name_file}', 'w') as file:
-        file.write(str(calculations.txt_Bill(sub_total, IVA, total)))
-    # Main Question
-    main_Question = calculations.question_Loop('This bill is going to be cancel with $? (Y/N): ')
-    # Agent of retention case:
-    agent_of_retention = calculations.question_Loop('Do you want to calc the retention?(Y/N): ')
-    calculations.main_Function(main_Question,agent_of_retention,sub_total,IVA,total)
+
+
+# while True:
+#     # Variables WIP
+#     name_file = f'Bill-{date}.txt'
+#     # TODO Create program to create an external PDF file.
+#     # Write method to write file in an external txt file.
+#     with open(f'Bills\\Bill-{sub_total:n}$ {name_file}', 'w') as file:
+#         file.write(str(calculations.txt_Bill(sub_total, IVA, total)))
